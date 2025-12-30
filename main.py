03853b6e93d3a131b803c0309a031001487aea57
Micstive <micahstivers@gmail.com>
2025-12-30 09:52:27 -0800

    added a game loop that displays the screen and that can close

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
+from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
+    pygame.init()
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        log_state()
+        screen.fill("black")
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
