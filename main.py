30474138f658621e1ff97def9d478511d443549d
Micstive <micahstivers@gmail.com>
2025-12-29 20:28:44 -0800

    set up main and some constants

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print("Starting Asteroids with pygame version: VERSION")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
