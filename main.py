da47e267a9df6ccb6c525407cdb30e0131df02ae
Micstive <micahstivers@gmail.com>
2025-12-31 10:26:07 -0800

    added some contants for Player class and added the player class

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         log_state()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
